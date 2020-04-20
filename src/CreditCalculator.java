5365911c0478e125831294da45fadb80e9cf86a7
Igor Volnukhin <zybu@live.ru>
2020-04-20 22:11:11 +0300

    Frist ver

--- src/CreditCalculator.java
+++ src/CreditCalculator.java
@@ -1,2 +1,9 @@
 public class CreditCalculator {
+    public int calculate(long summ, int countMonth, float percent){
+        float percentMonth = percent / 100 / 12;
+        float annuitant = (float) (percentMonth * (Math.pow((1 + percentMonth),countMonth))/(Math.pow((1+percentMonth),countMonth)-1));
+        long monthlyPayment = (long) (summ * annuitant);
+
+        return monthlyPayment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         long summ = 1_000_000; // Сумма кредита
-        int CountMonth = 12;   // Срок кредита
+        int countMonth = 12;   // Срок кредита
+        float percent = 9.99;  // Процентная ставка в процентах
+        long monthlyPayment;   // Ежемесячный платеж
+
+        CreditCalculator creditCalculator = new CreditCalculator();
+        monthlyPayment = creditCalculator(summ, countMonth, percent);
+
+        System.out.printf("Ежемесячный платёж = " + monthlyPayment);
     }
 }
