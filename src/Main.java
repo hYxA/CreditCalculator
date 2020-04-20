2a9a77f0b7966bcf31bab21bf71b1cc9ca3d1e46
Igor Volnukhin <zybu@live.ru>
2020-04-20 22:27:17 +0300

    Debug ver

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         long summ = 1_000_000; // Сумма кредита
         int countMonth = 12;   // Срок кредита
-        float percent = 9.99;  // Процентная ставка в процентах
-        long monthlyPayment;   // Ежемесячный платеж
+        float percent = 9.99F;  // Процентная ставка в процентах
+        float monthlyPayment;   // Ежемесячный платеж
 
         CreditCalculator creditCalculator = new CreditCalculator();
-        monthlyPayment = creditCalculator(summ, countMonth, percent);
+        monthlyPayment = creditCalculator.calculate(summ, countMonth, percent);
 
         System.out.printf("Ежемесячный платёж = " + monthlyPayment);
     }
